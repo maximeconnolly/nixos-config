b6a56ec19646dbab731584321fd9d7a01e8e32c0
Maxime Connolly <maxime@maximeconnolly.ca>
2025-12-15 10:58:20 -0400

    Change version to 25.11

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
  description = "mconnolly NixOS Flake";
  inputs = {
-   nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+   nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
    home-manager = {
-     url = "github:nix-community/home-manager/release-23.11";
+     url = "github:nix-community/home-manager/release-25.11";
      inputs.nixpkgs.follows = "nixpkgs";
    };
    
