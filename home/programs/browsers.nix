aee2e930930665f40fce0d63a0640355afeae891
Maxime Connolly <maxime@maximeconnolly.ca>
2026-01-19 20:05:22 -0400

    Fix missing desktop entry for chromium

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -15,4 +15,13 @@
       profiles.mstpierre = {};
     };
   };
+
+  xdg.desktopEntries.chromium = {
+    name = "Chromium";
+    genericName = "Web Browser";
+    exec = "chromium %U";
+    terminal = false;
+    categories = [ "Application" "Network" "WebBrowser" ];
+    mimeType = [ "text/html" "text/xml" "application/xhtml+xml" "application/vnd.mozilla.xul+xml" "text/mml" "x-scheme-handler/http" "x-scheme-handler/https" ];
+  };
 }
