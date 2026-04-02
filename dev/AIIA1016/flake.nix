d8c1fa0c92e36ae7f6dbdb5512ed8cc8bf260dcc
Maxime Connolly <maxime@maximeconnolly.ca>
2026-04-02 08:42:26 -0300

    Add environement for AIIA1016

---
+++ dev/AIIA1016/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Reproducible environment for AIIA1016";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pythonWithPackages = pkgs.python3.withPackages (ps: with ps; [
+          jupyterlab
+          numpy
+          pandas
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          PYTHONBIN = "${pythonWithPackages}/bin/python";
+          packages = [ pythonWithPackages ];
+          shellHook = "jupyter lab";
+        };
+      }
+    );
+}
